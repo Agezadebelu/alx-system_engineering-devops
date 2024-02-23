4f66ba8c36dfaa5d165f3df0bd5fe33782b62492
Agezadebelu <lateradeb2019@gmail.com>
2024-02-23 11:01:55 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+#!/usr/bin/puppet
+
+# Ensure that apt-get update is executed before attempting to install packages
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
+}
+
+# Install a specific version of Flask (2.1.0) using pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Exec['apt-get update'], # Ensure that apt-get update is executed before installing Flask
 }
