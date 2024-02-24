2098a1696bc4b6a5078712bbd21c4c90bee791fd
Agezadebelu <lateradeb2019@gmail.com>
2024-02-23 23:44:31 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,9 @@ package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
+# Install an especific version of Werkzeug (2.1.1)
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
